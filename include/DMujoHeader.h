f5079d1d0d4a4ff90f02317397495258a10bab54
LKDCC123 <dongchencheng@bit.edu.cn>
2023-04-20 21:33:05 +0800

    hehe

--- include/DMujoHeader.h
+++ include/DMujoHeader.h
@@ -10,6 +10,19 @@
 #include <DModelMaker.hpp>
 #include "MujocoBHR_api.h"
 
+#ifndef __PosCon 
+#define __PosCon 0
+#endif
+#ifndef __TrqCon 
+#define __TrqCon 1
+#endif
+#ifndef __ZeroGravity 
+#define __ZeroGravity 0
+#endif
+#ifndef __Gravity 
+#define __Gravity 1
+#endif
+
 #define _D_MUJOSIM_BEGIN namespace Dcc { namespace MUJOCO_FILES {
 #define _D_MUJOSIM_END }}
 #define _D_MUJO ::Dcc::MUJOCO_FILES::
